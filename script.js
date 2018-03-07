ba2f9ce1cbe724abb346bccc397a90ef145349f2
ashwini kasat <ashwini.kasat@gmail.com>
2018-03-07 00:33:46 -0800

    about page heading style change, all p tags font change team section linkedin link added. and some more style change.

--- script.js
+++ script.js
@@ -39,20 +39,20 @@ $(document).ready(function(){
         });
     });
 
-    myMap();
+    // myMap();
 });
 
 /* code for map , LatLng is location code*/
 
 /* 12 times user can zoom*/
 
-function myMap() {
-    var myCenter = new google.maps.LatLng(33.6167, -117.8975);
-    var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    var marker = new google.maps.Marker({position:myCenter});
-    marker.setMap(map);
-}
+// function myMap() {
+//     var myCenter = new google.maps.LatLng(33.6167, -117.8975);
+//     var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
+//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+//     var marker = new google.maps.Marker({position:myCenter});
+//     marker.setMap(map);
+// }
 
 
 
